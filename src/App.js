b1d88f289f303c41519c2533e794ff32f4907fa8
Felix-fale <falefelix23@gmail.com>
2023-01-10 02:06:23 +0000

    feat: Styling the Chat message interface

--- src/App.js
+++ src/App.js
@@ -12,10 +12,18 @@ function App() {
       </aside>
       <section className="chatbox">
         <div className="chat-log">
-          <div className="chat-message chatgpt">
+          <div className="chat-message ">
             <div className="chat-message-center">
               <div className="avatar"></div>
               <div className="message">Hello word</div>
+            </div>
+          </div>
+          <div className="chat-message chatgpt">
+            <div className="chat-message-center">
+              <div className="avatar chatgpt">
+
+              </div>
+              <div className="message">i am an AI</div>
             </div>
           </div>
         </div>
